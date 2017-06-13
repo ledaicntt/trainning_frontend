257f88d19784156af1a050f2f4a9d3f9f1efaa84
Le Van Dai <ledaicntt@gmail.com>
2017-06-13 17:34:43 +0700

    day5

---
+++ day5/dai/Gruntfile.js
@@ -0,0 +1,64 @@
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            html:{
+                files: [ 'html/**' ],
+                tasks: ['includes']
+            },
+            css:{
+                files: [ 'css/scss/**'],
+                tasks: ['sass']
+            }
+        },
+
+        sass: {
+            dev: {
+                files: {
+                    'css/style.css': 'css/scss/style.scss'
+                }
+            }
+        },
+
+        includes: {
+            build: {
+                cwd: 'html',
+                src: ['*.html'],
+                dest: '.',
+                options: {
+                    flatten: true,
+                    includePath: 'html/pages',
+                    banner: '<!-- Site built using grunt includes! -->\n'
+                }
+            }
+        },
+
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src: [
+                        'css/*.css',
+                        '*.html'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    notify: false,
+                    server: './'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-includes');
+
+    // Default task(s).
+    // grunt.registerTask('default', ['browserSync', 'sass']);
+    // grunt.registerTask('default', ['includes']);
+    grunt.registerTask('default', ['browserSync', 'watch']);
+};
