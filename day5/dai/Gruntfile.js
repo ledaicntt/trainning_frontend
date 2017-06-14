0f03602ced252bae71d1db86a19c6a1aa443d053
Le Van Dai <ledaicntt@gmail.com>
2017-06-14 18:10:29 +0700

    day5

--- day5/dai/Gruntfile.js
+++ day5/dai/Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function (grunt) {
         pkg: grunt.file.readJSON('package.json'),
         watch: {
             html:{
-                files: [ 'html/**' ],
+                files: [ 'html/**',  'html/main/**'],
                 tasks: ['includes']
             },
             css:{
